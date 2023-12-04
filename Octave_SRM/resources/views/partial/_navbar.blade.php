aba63d4fd9e4d2caea9da061796a8f0ba121d2f2
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2023-12-04 14:47:30 +0700

    fix

---
+++ Octave_SRM/resources/views/partial/_navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/"><h4> Octave Allegro</h4><h6>Report System</h6></a>
+      {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button> --}}
+    </div>
+        <ul class="nav navbar-nav navbar-right justify-content-end">
+          @guest
+              <li class="active nav-item">
+                  <a class="" href="{{ route('login') }}"><button class="btn btn-primary">Sign In</button></a>
+              </li>
+              <li class="active nav-item">
+                  <a class="nav-link" href="{{ route('register') }}"><button class="btn btn-success">Sign Up</button></a>
+              </li>
+          @else
+              <li>
+                  <a href="/"></span>Welcome, {{ auth()->user()->username }}</a>
+              </li>
+              <li class="active nav-item">
+                  <a class="nav-link" href="{{ route('logout') }}"><button class="btn btn-danger">Sign Out</button></a>
+              </li>
+          @endguest
+      </ul>
+</div>
+</nav>
--- Octave_SRM/resources/views/partial/navbar.blade.php
+++ Octave_SRM/resources/views/partial/navbar.blade.php
@@ -1,27 +1,36 @@
-<nav class="navbar navbar-expand-lg navbar-light">
-<div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="/"><h4> Octave Allegro</h4><h6>Report System</h6></a>
-      {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button> --}}
-    </div>
-        <ul class="nav navbar-nav navbar-right">
-          @guest
-              <li class="active nav-item">
-                  <a class="" href="{{ route('login') }}"><button class="btn btn-primary"><span></span>Sign In</button></a>
-              </li>
-              <li class="active nav-item">
-                  <a class="nav-link" href="{{ route('register') }}"><button class="btn btn-success"><span></span>Sign Up</button></a>
-              </li>
-          @else
-              <li>
-                  <a href="/"></span>Welcome, {{ auth()->user()->username }}</a>
-              </li>
-              <li class="active nav-item">
-                  <a class="nav-link" href="{{ route('logout') }}"><button class="btn btn-danger"><span></span>Sign Out</button></a>
-              </li>
-          @endguest
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">
+      <h4>Octave Allegro</h4>
+      <h6>Report System</h6>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        @guest
+          <li class="nav-item active">
+            <a class="nav-link" href="{{ route('login') }}">
+              <button class="btn btn-primary">Sign In</button>
+            </a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="{{ route('register') }}">
+              <button class="btn btn-success">Sign Up</button>
+            </a>
+          </li>
+        @else
+          <li class="nav-item">
+            <a class="nav-link" href="/"><span>Welcome, {{ auth()->user()->username }}</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="{{ route('logout') }}">
+              <button class="btn btn-danger">Sign Out</button>
+            </a>
+          </li>
+        @endguest
       </ul>
-</div>
+    </div>
+  </div>
 </nav>
