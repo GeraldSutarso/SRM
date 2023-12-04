611ff64c2d75c829191d2b97bed396b3842699f0
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2023-12-04 15:43:13 +0700

    a naxcasdw

--- Octave_SRM/resources/views/auth/login.blade.php
+++ Octave_SRM/resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
       <div class="row justify-content-center">
           <div class="col-md-8">
               <div class="card">
-                  <div class="card-header">Login</div>
+                  <div class="card-header"><center><b>Sign In</b></center></div>
                   <div class="card-body">
   
                       <form action="{{ route('login.post') }}" method="POST">
--- Octave_SRM/resources/views/auth/registration.blade.php
+++ Octave_SRM/resources/views/auth/registration.blade.php
@@ -6,7 +6,7 @@
       <div class="row justify-content-center">
           <div class="col-md-8">
               <div class="card">
-                  <div class="card-header">Register</div>
+                  <div class="card-header"><center><b>Sign Up</b></center></div>
                   <div class="card-body">
   
                       <form action="{{ route('register.post') }}" method="POST">
