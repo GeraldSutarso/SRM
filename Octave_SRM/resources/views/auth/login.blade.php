8ae30b8cf9274fbae8cc6b4f480884f9ef0d6ea7
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2023-12-05 16:08:51 +0700

    ternyata yang tadi gk error, cmn human error

--- Octave_SRM/resources/views/auth/login.blade.php
+++ Octave_SRM/resources/views/auth/login.blade.php
@@ -12,9 +12,9 @@
                       <form action="{{ route('login.post') }}" method="POST">
                           @csrf
                           <div class="form-group row">
-                              <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address:</label>
+                              <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address:</label>
                               <div class="col-md-6">
-                                  <input type="text" id="email_address" class="form-control" placeholder="Enter your email" name="email" required autofocus>
+                                  <input type="text" id="email" class="form-control" placeholder="Enter your email" name="email" required autofocus>
                                   @if ($errors->has('email'))
                                       <span class="text-danger">{{ $errors->first('email') }}</span>
                                   @endif
