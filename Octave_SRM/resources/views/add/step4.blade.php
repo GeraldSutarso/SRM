19f14d420d063080784c5c8f09b3e4715a00e804
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2023-12-11 10:49:04 +0700

    Revert "Revert "severity (step4) done""
    This reverts commit 026dff435466f7b4c2909e855d1cb6f00b568ace.

--- Octave_SRM/resources/views/add/step4.blade.php
+++ Octave_SRM/resources/views/add/step4.blade.php
@@ -1,54 +1,59 @@
 @extends('layout.main') 
 @section('content')
 <div class="container">
-  <h2>Step 4: Fill In Risk Identification</h2>
+  <h2>Step 4: Fill In Risk Identification</h2><br><br>
   <form method="POST" action="{{ route('step4.post') }}">
     @csrf
     <table class="table">
       <tbody>
-        <tr>
-          <td>Threat Properties</td>
+        <tr><td></td>
+          <td><h5>Threat Properties</h5></td>
         </tr>
         <tr>
           <td>Area of Concern</td>
-          <td><textarea placeholder="Input Area of Concern" name="concern" class="form-control" required></textarea></td>
+          <td><textarea placeholder="Input Area of Concern" name="area_of_concern" class="form-control" required>{{ old('area_of_concern') }}</textarea></td>
         </tr>
         <tr>
           <td>Actor</td>
-          <td><textarea placeholder="Input Actor" name="actor" class="form-control" required></textarea></td>
+          <td><input placeholder="Input Actor" name="actor" class="form-control" required value="{{ old('actor') }}"></td>
         </tr>
         <tr>
           <td>Objective</td>
-          <td><textarea placeholder="Input Objective" name="objective" class="form-control" required></textarea></td>
+          <td><input placeholder="Input Objective" name="objective" class="form-control" required value="{{ old('objective') }}"></td>
         </tr>
         <tr>
           <td>Motive</td>
-          <td><textarea placeholder="Input Motive" name="motive" class="form-control" required></textarea></td>
+          <td><input placeholder="Input Motive" name="motive" class="form-control" value="{{ old('motive') }}" required></td>
         </tr>
         <tr>
           <td>Result</td>
-          <td><textarea placeholder="Input Result" name="result" class="form-control" required></textarea></td>
+          <td><input placeholder="Input Result" name="result" class="form-control" value="{{ old('result') }}" required></td>
         </tr>
         <tr>
           <td>Security Needs</td>
-          <td><textarea placeholder="Input Security Needs" name="needs" class="form-control" required></textarea></td>
+          <td><textarea placeholder="Input Security Needs" name="security_needs" class="form-control" value="{{ old('security_needs') }}" required></textarea></td>
         </tr>
         <tr>
-          <td>Possibility</td>
-          <td><textarea placeholder="Input Possibility" name="possibility" class="form-control" required></textarea></td>
+          <td>Probability</td>
+          <td><select id="probability" class="form-control" name="probability" required>
+            <option value="">Select Probability Chances</option>
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+        </select></td>
         </tr>
         <tr>
           <td>Consequence</td>
-          <td><textarea placeholder="Input Consequence" name="consequence" class="form-control" required></textarea></td>
+          <td><textarea placeholder="Input Consequence" name="consequences" class="form-control" required>{{ old('consequence') }}</textarea></td>
         </tr>
         <tr>
-          <td>Controll</td>
-          <td><textarea placeholder="Input Control" name="control" class="form-control" required></textarea></td>
+          <td>Control</td>
+          <td><textarea placeholder="Input Control" name="control" class="form-control" required>{{ old('control') }}</textarea></td>
         </tr>
       </tbody>
     </table>
     <div class="form-group">
-      <a href="{{ route('home') }}" class="btn btn-danger">Cancel</a>
+      <a href="{{ route('step3') }}" class="btn btn-secondary">Back</a>
       <button type="submit" class="btn btn-primary">Next</button>
     </div>
   </form>
