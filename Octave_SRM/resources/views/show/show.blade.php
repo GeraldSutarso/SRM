1dd835ec8ba63dc8668688b4c6d4b18e70cec296
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2023-12-12 12:10:25 +0700

    Update show.blade.php

--- Octave_SRM/resources/views/show/show.blade.php
+++ Octave_SRM/resources/views/show/show.blade.php
@@ -1,12 +1,63 @@
 @extends('layout.main')
 
 @section('content')
+<<<<<<< HEAD
+<div class="container">
+    <h3>Asset Details</h3>&ensp;
+=======
 <div class="container bg-white"><br>
     <h3><center>Asset Details</center></h3><br><br>
+>>>>>>> b37a2530ffadbaec4b61c2122f0e8c7d821b5f2e
 
 {{-- priority dan severity biar gw yg buat soalnya agak beda --}}
 
     {{-- ini kyk risk identification tapi asset, mapping dll --}}
+
+    {{-- Asset Informations Table --}}
+    <h4>Asset Information</h4>
+    <table class="table">
+        <tr>
+            <td><h6>Critical Asset :</h6></td>
+            <td>{{ $asset->asset_name }}</td>
+        </tr>
+        <tr>
+            <td><h6>Rationale for Selection :</h6></td>
+            <td>{{ $asset->rationale_for_select }}</td>
+        </tr>
+        <tr>
+            <td><h6>Description :</h6></td>
+            <td>{{ $asset->asset_desc }}</td>
+        </tr>
+        <tr>
+            <td><h6>Owner :</h6></td>
+            <td>{{ $asset->owner }}</td>
+        </tr>
+        <tr>
+            <td><h6>Department :</h6></td>
+            <td>{{ $asset->a_department }}</td>
+        </tr>
+    </table>
+
+    {{-- Security Requirements Table --}}
+    <h5>Security Requirement</h5>
+    <table class="table">
+        <tr>
+            <td><h6>Confidentiality :</h6></td>
+            <td>{{ $asset->SR_confidentiality }}</td>
+        </tr>
+        <tr>
+            <td><h6>Integrity :</h6></td>
+            <td>{{ $asset->SR_integrity }}</td>
+        </tr>
+        <tr>
+            <td><h6>Availability :</h6></td>
+            <td>{{ $asset->SR_availability }}</td>
+        </tr>
+        <tr>
+            <td><h6>Most Important Security Requirement :</h6></td>
+            <td>{{ $asset->most_important_SR }}</td>
+        </tr>
+    </table>
 
     {{-- Risk Identifications Table --}}
     <h5>Risk Identifications</h5>
