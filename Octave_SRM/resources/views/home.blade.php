0f838c532c0a6e1bb9aaff84137d6ef65c16d87a
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2023-12-12 08:14:30 +0700

    button styling

--- Octave_SRM/resources/views/home.blade.php
+++ Octave_SRM/resources/views/home.blade.php
@@ -48,27 +48,30 @@
                                 <td>{{ $asset->asset_name }}</td>
                                 <td>{{ $asset->a_department }}</td>
                                 <td>{{ $asset->created_at }}</td>
-                                <td><button class="btn btn-success" href="{{ route('show') }}" id="button-show">
+                                <td>
+                                <div style="display: flex; justify-content: space-around; align-items: center; gap: 1px;">
+                                    <a class="btn btn-success d-inline-block" href="{{ route('show') }}" id="button-show">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filetype-pdf" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z"/>
                                       </svg>
-                                </button>&nbsp;&nbsp;
-                                  <button class="btn btn-primary" href="{{ route('update') }}" id="button-update">
+                                    </a>
+                                    <a class="btn btn-primary d-inline-block" href="{{ route('update') }}" id="button-update">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
                                       </svg>
-                                  </button>&nbsp;&nbsp;
-                                  <form action ="{{ route('delete.asset', $asset->asset_id) }}" method ="POST">
-                                    @csrf 
+                                    </a>
+                                    <form action="{{ route('delete.asset', $asset->asset_id) }}" method="POST" class="d-inline-block">
+                                    @csrf
                                     @method('DELETE')
-                                  <button class="btn btn-danger" type ="submit" id="button-delete" onclick ="return confirm('Are you sure you want to delete this asset and all its attributes?')">
+                                    <button class="btn btn-danger" type="submit" id="button-delete" onclick="return confirm('Are you sure you want to delete this asset and all its attributes?')">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                                       </svg>
                                 </button></form>
-                            </td>
+                                </div>
+                                </td>
                                 <!-- Add other data columns as needed -->
                             </tr>
                         @endforeach
