97d26c44fda8eb75567ce3c5fdeeae0c7c6a29d1
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2023-12-13 11:52:40 +0700

    Update step6.blade.php

--- Octave_SRM/resources/views/add/step6.blade.php
+++ Octave_SRM/resources/views/add/step6.blade.php
@@ -2,13 +2,15 @@
 @section('content')
 {{-- {{ dd(session()->all()) }} --}}
     <div class="container">
-        <center><h2>Save the data?</h2></center>
+        <center><h2>Are you sure you want to save the data?</h2></center>
+        <br><br>
         <form method="POST" action="{{ route('add.save') }}">
             @csrf
-            <div class="form-group">
+            <center><div class="form-group">
                 <a href="{{ route('step5') }}" class="btn btn-secondary">Back</a>
                 <button type="submit" class="btn btn-success">Save</button>
             </div>
+            </center>
         </form>
     </div>
 @endsection
